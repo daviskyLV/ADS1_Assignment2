b62d79a08f3084b71c32576ace6d355be5df00fb
daviskyLV <ledaviskylv@gmail.com>
2023-05-10 01:48:23 +0200

    TreeNode and Tree tests

--- test/BinaryTreeNodeTest.java
+++ test/BinaryTreeNodeTest.java
@@ -5,36 +5,54 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BinaryTreeNodeTest {
+    private BinaryTreeNode<Integer> node;
+    private final BinaryTreeNode<Integer> leftChild = new BinaryTreeNode<>(2);
+    private final BinaryTreeNode<Integer> rightChild = new BinaryTreeNode<>(10);
 
     @BeforeEach
     void setUp() {
+        node = new BinaryTreeNode<>(5);
     }
 
     @AfterEach
     void tearDown() {
+        node = null;
     }
 
     @Test
     void setElement() {
+        node.setElement(1);
+        assertEquals(1, node.getElement());
     }
 
     @Test
     void getElement() {
+        assertEquals(5, node.getElement());
     }
 
     @Test
     void addLeftChild() {
+        node.addLeftChild(leftChild);
+        assertEquals(leftChild, node.getLeftChild());
+        assertNotEquals(null, node.getLeftChild());
     }
 
     @Test
     void addRightChild() {
+        node.addRightChild(rightChild);
+        assertEquals(rightChild, node.getRightChild());
+        assertNotEquals(null, node.getRightChild());
     }
 
     @Test
     void getLeftChild() {
+        assertNull(node.getRightChild());
+        assertNotEquals(leftChild, node.getRightChild());
     }
 
     @Test
     void getRightChild() {
+        assertNull(node.getRightChild());
+        assertNotEquals(rightChild, node.getRightChild());
     }
 }
--- test/BinaryTreeTest.java
+++ test/BinaryTreeTest.java
@@ -5,33 +5,71 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BinaryTreeTest {
+    private BinaryTree<Integer> tree;
+    private BinaryTreeNode<Integer> node;
+    private BinaryTreeNode<Integer> node2;
 
     @BeforeEach
     void setUp() {
+        tree = new BinaryTree<>();
+        node = new BinaryTreeNode<>(16);
+        node2 = new BinaryTreeNode<>(25);
     }
 
     @AfterEach
     void tearDown() {
+        tree = null;
+        node = null;
+        node2 = null;
     }
 
     @Test
     void getRoot() {
+        assertNull(tree.getRoot());
+        assertNotEquals(new BinaryTreeNode<>(1), tree.getRoot());
     }
 
     @Test
     void setRoot() {
+        var a = new BinaryTreeNode<>(1);
+        tree.setRoot(a);
+        assertNotEquals(null, tree.getRoot());
+        assertEquals(a, tree.getRoot());
     }
 
     @Test
-    void isEmpty() {
+    void isEmptyTrue() {
+        assertTrue(tree.isEmpty());
     }
 
     @Test
-    void size() {
+    void isEmptyFalse() {
+        tree.setRoot(node);
+        assertFalse(tree.isEmpty());
     }
 
     @Test
-    void contains() {
+    void sizeZero() {
+        assertEquals(0, tree.size());
+        tree.setRoot(node);
+        assertEquals(1, tree.size());
+    }
+
+    @Test
+    void sizeOne() {
+        tree.setRoot(node);
+        assertEquals(1, tree.size());
+    }
+
+    @Test
+    void containsFalse() {
+        assertFalse(tree.contains(1));
+    }
+
+    @Test
+    void containsTrue() {
+        tree.setRoot(node);
+        assertFalse(tree.contains(16));
     }
 
     @Test
@@ -47,10 +85,35 @@ class BinaryTreeTest {
     }
 
     @Test
-    void levelOrder() {
+    void levelOrderEmpty() {
+        assertEquals(0, tree.levelOrder().size());
     }
 
     @Test
-    void height() {
+    void levelOrderFilled() {
+        tree.setRoot(node);
+        node.addRightChild(node2);
+        var ordering = tree.levelOrder();
+        assertEquals(2, ordering.size());
+        assertEquals(16, ordering.get(0));
+        assertEquals(25, ordering.get(1));
+    }
+
+    @Test
+    void heightEmpty() {
+        assertEquals(-1, tree.height());
+    }
+
+    @Test
+    void heightSingle() {
+        tree.setRoot(node);
+        assertEquals(0, tree.height());
+    }
+
+    @Test
+    void heightDouble() {
+        tree.setRoot(node);
+        node.addRightChild(node2);
+        assertEquals(1, tree.height());
     }
 }
