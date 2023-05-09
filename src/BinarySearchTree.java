3365edbf0145ca25a6498a539480c60e44685354
daviskyLV <ledaviskylv@gmail.com>
2023-05-10 00:10:57 +0200

    findMin, findMax and insert()

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,6 +1,41 @@
 public class BinarySearchTree<T> extends BinaryTree<T> {
     public boolean insert(T element) {
-        return false;
+        if (root == null) {
+            root = new BinaryTreeNode<>(element);
+            return true;
+        }
+
+        return insert(element, root);
+    }
+
+    private boolean insert(T element, BinaryTreeNode<T> node) {
+        if (node.getElement().equals(element))
+            return false;
+
+        BinaryTreeNode<T> left = node.getLeftChild();
+        BinaryTreeNode<T> right = node.getRightChild();
+        // idk how to compare for unspecified types
+        if (element > node.getElement()) {
+            if (right == null) {
+                node.addRightChild(
+                        new BinaryTreeNode<>(element)
+                );
+                return true;
+            }
+            return insert(element, right);
+        }
+
+        if (element < node.getElement()) {
+            if (left == null) {
+                node.addLeftChild(
+                        new BinaryTreeNode<>(element)
+                );
+                return true;
+            }
+            return insert(element, left);
+        }
+
+        return false; // bug?
     }
 
     public boolean removeElement(T element) {
@@ -8,11 +43,33 @@ public class BinarySearchTree<T> extends BinaryTree<T> {
     }
 
     public T findMin() {
-        return null;
+        if (root == null)
+            return null;
+
+        return findMin(root);
+    }
+
+    private T findMin(BinaryTreeNode<T> node) {
+        BinaryTreeNode<T> left = node.getLeftChild();
+        if (left != null)
+            return findMin(left);
+
+        return node.getElement();
     }
 
     public T findMax() {
-        return null;
+        if (root == null)
+            return null;
+
+        return findMax(root);
+    }
+
+    private T findMax(BinaryTreeNode<T> node) {
+        BinaryTreeNode<T> right = node.getRightChild();
+        if (right != null)
+            return findMax(right);
+
+        return node.getElement();
     }
 
     public void rebalance() {
