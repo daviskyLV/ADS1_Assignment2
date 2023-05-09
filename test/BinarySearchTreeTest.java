141dc87a6ece8d1abb4c65f935bb49377982311b
daviskyLV <ledaviskylv@gmail.com>
2023-05-10 01:56:20 +0200

    More tests

--- test/BinarySearchTreeTest.java
+++ test/BinarySearchTreeTest.java
@@ -5,29 +5,69 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BinarySearchTreeTest {
+    private BinarySearchTree<Integer> bst;
 
     @BeforeEach
     void setUp() {
+        bst = new BinarySearchTree<>();
     }
 
     @AfterEach
     void tearDown() {
+        bst = null;
     }
 
     @Test
-    void insert() {
+    void insertOne() {
+        assertTrue(bst.insert(123));
+        assertTrue(bst.contains(123));
     }
 
     @Test
-    void removeElement() {
+    void insertDuplicate() {
+        assertTrue(bst.insert(123));
+        assertTrue(bst.contains(123));
+        assertFalse(bst.insert(123));
+    }
+
+    @Test
+    void removeElementEmpty() {
+        assertFalse(bst.removeElement(123));
+    }
+
+    @Test
+    void removeElementMatches() {
+        bst.insert(123);
+        assertTrue(bst.removeElement(123));
+        assertFalse(bst.contains(123));
     }
 
     @Test
     void findMin() {
+        bst.insert(123);
+        bst.insert(12);
+        bst.insert(14);
+        assertEquals(12, bst.findMin());
+        assertNotEquals(14, bst.findMin());
+    }
+
+    @Test
+    void findMinEmpty() {
+        assertNull(bst.findMin());
     }
 
     @Test
     void findMax() {
+        bst.insert(123);
+        bst.insert(12);
+        bst.insert(14);
+        assertEquals(123, bst.findMax());
+        assertNotEquals(14, bst.findMax());
+    }
+
+    @Test
+    void findMaxEmpty() {
+        assertNull(bst.findMax());
     }
 
     @Test
